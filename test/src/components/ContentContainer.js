64744cf9dd6bd00a69dc76f747750796160526ed
Gaute Brandser <gautbra@stud.ntnu.no>
2022-02-22 14:37:51 +0100

    components tbc

---
+++ test/src/components/ContentContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ContentHeader from './ContentHeader'
+const ContentContainer = () => {
+  return (
+    <div className='gamecontent'>
+      <ContentHeader></ContentHeader>
+    </div>
+  )
+}
+
+export default ContentContainer
---
+++ test/src/components/ContentHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContentHeader = () => {
+  return (
+    <div className='contentHeader'>
+      <p>content header</p>
+    </div>
+  )
+}
+
+export default ContentHeader
