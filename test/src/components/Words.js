8a7802ed08ba0a2a55064c6adc450202c4ab7afe
Gaute Brandser <gautbra@stud.ntnu.no>
2022-02-28 09:00:27 +0100

    cleanup from last commit (#9)

---
+++ test/src/components/Words.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Words = () => {
+  return (
+    <div className='word-grid'>
+      <div>
+        <p className='word'> </p>
+      </div>
+      <div>
+        <p className='word'>har</p>
+      </div>
+      <div>
+        <p className='word'>fornøyd</p>
+      </div>
+      <div>
+        <p className='word'>leilighet</p>
+      </div>
+      <div>
+        <p className='word'>mange</p>
+      </div>
+      <div>
+        <p className='word'>med</p>
+      </div>
+    </div>
+  )
+}
+
+export default Words
